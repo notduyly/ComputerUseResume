1cc95287f46e6d2276304c4c2d9faf3ccbd6f112
notduyly <162235323+notduyly@users.noreply.github.com>
2025-08-17 17:38:01 -0700

    feat: added application link form

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,13 +1,13 @@
+import { Form } from "react-bootstrap";
 import Orb from "../components/orb/Orb";
+import { useState } from "react";
 
 export const HomePage = () => {
+  const [appLink, setAppLink] = useState('');
+
   return (
     <>
-      <p>Home Page</p>
-      <div style={{ 
-        width: '100%', 
-        height: '65vh', 
-      }}>
+      <div style={{ width: '100%', height: '60vh', marginTop: '100px' }}>
         <Orb
           hoverIntensity={0.2}
           rotateOnHover={true}
@@ -15,6 +15,21 @@ export const HomePage = () => {
           forceHoverState={false}
         />
       </div>
+      <div style={{ width: '35%', margin: '30px auto'}}>
+        <Form>
+          <Form.Group className=''>
+            <Form.Control 
+              type='text' 
+              value={appLink} 
+              onChange={(e) => setAppLink(e.target.value)}
+              placeholder='Application Link' 
+              style={{ backgroundColor: '#B8B8D9' }}
+              required
+            />
+          </Form.Group>
+        </Form>
+      </div>
+      
     </>
   );
 };
